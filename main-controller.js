630486feb5f1bc0134b60e6e0b95d1c8ce52fdb1
ramesh199823 <vemburamesh376@gmail.com>
2023-04-11 11:13:27 +0530

    S

--- main-controller.js
+++ main-controller.js
@@ -5,23 +5,35 @@ module.controller("exampleCtrl", function($scope) {
     $scope.name = 'MM';
     $scope.dataSource = {
         column : [{ field: 'name1',
-                   type: 'input',
+                   type: 'number',
+                   editable: false,
                    }, {field: 'data',
-                    type: 'input'
-                   }],
+                    type: 'input',
+                    editable: true
+                   },
+                   {
+                    field: 'select',
+                    type: 'select',
+                    editable: true,
+                    dropdowndata: ['anish','vembu', 'mani']
+                   }
+                ],
         data : [{
         'name1': 'sabari',
         'data': 'sabari',
-        'editable': false
+        'editable': false,
+        'select': 'anish'
     },
     {
         'name1': 'sabari2',
         'data': 'sabari',
+        'select': 'anish',
         editable: false
     },
     {
         'name1': 'sabari3',
         'data': 'sabari',
+        'select': 'anish',
         editable: false
     }]
 }
