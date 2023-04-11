aff172837ef556eed202a26274f3b55f1acb893f
manikandanmohan10 <manikandan.mohan@softsuave.com>
2023-04-11 10:37:15 +0530

    s

--- main-directive.js
+++ main-directive.js
@@ -11,27 +11,24 @@ angular.module('myApp', ['dndLists'])
       console.log(scope)
       setTimeout(function() {
         $('#mytable').DataTable(); // Initialize DataTables plugin after AngularJS data is populated
-
-        // Add jQuery UI Drag and Drop functionality
-        $( "#mytable tbody" ).sortable({
-          helper: 'clone',
-          update: function(event, ui) {
-            // Get the new order of the rows
-            var newOrder = $(this).sortable('toArray');
-
-            // Update the scope data with the new order
-            scope.$apply(function() {
-              scope.data = newOrder.map(function(id) {
-                return scope.data.find(function(item) {
-                  return item.id === parseInt(id);
-                });
-              });
-            });
-          }
-        }).disableSelection();
+        $('#mytable').tableDnD(); // Initialize TableDnD plugin for row drag-and-drop
+        $('#mytable').colResizable({liveDrag: true}); // Initialize colResizable plugin for column drag-and-drop
         
       }, 0);
     },
-    templateUrl: 'table.html'
+    templateUrl: 'table.html',
+    controller: function($scope) {
+      $scope.editRow = function(row) {
+        // update the row
+        row.editable = !row.editable
+        row = row;
+      };
+      $scope.deleteRow = function(row) {
+        // delete the row
+      };
+      $scope.$watch('data', function() {
+        // update the table
+      });
+    }
   };
 });
